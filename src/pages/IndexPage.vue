fe150808cc2053937dc54fc9d0a45d975b71539d
Anderson Almeida <anderson.almeida@pmweb.com.br>
2023-06-22 18:41:15 -0300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    {{ t("general.adult", 2) }}
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+</script>
